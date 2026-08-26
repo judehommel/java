9e6233c0d857bf30ab07cba6e793d89d36dba80a
Jude Hommel <judeduder@gmail.com>
2026-08-25 18:41:53 -0700

    first commit

---
+++ countThree.java
@@ -0,0 +1,10 @@
+public class countThree {
+    public static boolean countThree() {
+        System.out.println("Hello");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        countThree();
+    }
+}
